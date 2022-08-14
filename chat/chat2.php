b9ae8efef910433cb1904e2da4fd22ea2c3d046a
xfusion <110038219+xfusionalg@users.noreply.github.com>
2022-08-14 15:57:24 +0200

    Delete chat2.php

--- chat/chat2.php
+++
@@ -1,5 +0,0 @@
-<?php
-error_reporting(0);
-file_put_contents("messages.txt", " Username: ". $_POST["username"]. " Message: ". $_POST["message"]. "\n", FILE_APPEND);
-header('Location: chat.html'); //your site
-?>
